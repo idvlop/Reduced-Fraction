672d339582290031916c6a9559beb79ea6ee042f
idvlop <38253137+idvlop@users.noreply.github.com>
2023-12-11 05:23:59 +0500

    refactored + gcd without recursive

--- Rational.cs
+++ Rational.cs
@@ -13,10 +13,16 @@ namespace Incapsulation.RationalNumbers
             Numerator = 0;
             Denominator = 0;
             IsNan = false;
-
-            Numerator = numerator;
             Denominator = numerator == 0 ? 1 : denominator;
-            IsNan = Denominator == 0;
+            Numerator = numerator;
+            IsNan = denominator == 0;
+            var isInproper = !IsNan && numerator != 0 && denominator != 1 && numerator != 1;
+            if (isInproper)
+            {
+                var rat = ToProper(numerator, denominator);
+                Numerator = rat.Numerator;
+                Denominator = rat.Denominator;
+            }
         }
 
         public int Numerator { get; set; }
@@ -28,11 +34,13 @@ namespace Incapsulation.RationalNumbers
             if (left.IsNan || right.IsNan)
                 throw new ArgumentException();
 
-            var gcd = GCD(left.Denominator, right.Denominator);
-            var lcm1 = LCM(gcd, left.Numerator);
-            var lcm2 = LCM(gcd, right.Numerator);
+            var lcm = LCM(left.Denominator, right.Denominator);
+            var gcd1 = GCD(lcm, left.Numerator);
+            var gcd2 = GCD(lcm, right.Numerator);
 
-            return new Rational(left.Numerator * lcm1 + right.Numerator * lcm2, gcd);
+            var newNumerator = left.Numerator * gcd1 + right.Numerator * gcd2;
+
+            return ToProper(newNumerator, lcm);
         }
 
         public static Rational operator *(Rational left, Rational right)
@@ -43,7 +51,7 @@ namespace Incapsulation.RationalNumbers
             var newNom = left.Numerator * right.Numerator;
             var newDen = left.Denominator * right.Denominator;
 
-            return new Rational(newNom, newDen);
+            return ToProper(newNom, newDen);
         }
 
         public static Rational operator -(Rational left, Rational right)
@@ -51,38 +59,82 @@ namespace Incapsulation.RationalNumbers
             if (left.IsNan || right.IsNan)
                 throw new ArgumentException();
 
-            var gcd = GCD(left.Denominator, right.Denominator);
-            var lcm1 = LCM(gcd, left.Numerator);
-            var lcm2 = LCM(gcd, right.Numerator);
+            var lcm = LCM(left.Denominator, right.Denominator);
+            var gcd1 = GCD(lcm, left.Numerator);
+            var gcd2 = GCD(lcm, right.Numerator);
 
-            return new Rational(left.Numerator * lcm1 - right.Numerator * lcm2, gcd);
+            var newNumerator = left.Numerator * gcd1 - right.Numerator * gcd2;
+
+            return ToProper(newNumerator, lcm);
         }
 
         public static Rational operator /(Rational left, Rational right)
         {
-            if (left.IsNan || right.IsNan)
+            if (left.IsNan && right.IsNan)
                 throw new ArgumentException();
+            else if (right.IsNan || left.IsNan)
+                return GetNan;
 
             var newNom = left.Numerator * right.Denominator;
             var newDen = left.Denominator * right.Numerator;
 
-            return new Rational(newNom, newDen);
+            if (newDen < 0)
+            {
+                newNom = -newNom;
+                newDen = -newDen;
+            }
+            return ToProper(newNom, newDen);
         }
 
-        public static implicit operator double(Rational rational) => rational.IsNan ? double.NaN : (double) rational.Numerator / rational.Denominator;
+        public static implicit operator double(Rational rational) => 
+            rational.IsNan
+            ? double.NaN
+            : rational.Numerator / (double) rational.Denominator;
+
+        public static explicit operator int(Rational rational)
+        {
+            if(rational.IsNan)
+                throw new Exception();
+
+            rational = ToProper(rational.Numerator, rational.Denominator);
+            
+            return rational.Denominator == 1 ? rational.Numerator :
+                throw new Exception();
+        }
 
         public static implicit operator Rational(int x) => new Rational(x);
 
-        // НОК
+        // НОД - Наибольший общий делитель
+        // GCD - Greatest common divisor
         private static int GCD(int a, int b)
         {
-            return b != 0 ? GCD(b, a % b) : a; ;
+            if (a == 1 || b == 1 )
+                return 1;
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a | b;
         }
 
-        // НОД
+        // НОК - Наименьшее общее кратное
+        // LCM - Least common multiple
         private static int LCM(int a, int b)
         {
             return a / GCD(a, b) * b;
         }
+
+        private static Rational ToProper(int a, int b)
+        {
+            var gcd = Math.Abs(GCD(a, b));
+            return new Rational(a / gcd, b / gcd);
+        }
+
+        private static Rational GetNan = new Rational(0, 0);
     }
 }
